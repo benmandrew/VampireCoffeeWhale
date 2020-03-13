d59ac6730531e28a3a65de83419695fd428abbdb
Mirrorworld <ba405@cam.ac.uk>
2020-03-13 14:44:51 +0000

    no rotation and single jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour {
-    public float speed = 100.0f;
-    public float velX;
+    public float speed = 5.0f;
+    private float velX;
     private Rigidbody2D rb2D;
+    private bool hasJumped;
     
     void Start() {
         rb2D = GetComponent<Rigidbody2D>();
+        rb2D.freezeRotation = true;
         velX = 0.0f;
+        hasJumped = false;
     }
 
     void Update() {
@@ -22,15 +25,20 @@ public class Player : MonoBehaviour {
             velX = 1.0f;
         }
 
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKeyDown(KeyCode.W) && !hasJumped) {
             rb2D.velocity = new Vector2(
                 rb2D.velocity.x, 10f);
+            hasJumped = true;
         }
     }
 
     void FixedUpdate() {
         rb2D.velocity = new Vector2(
-            velX * speed * Time.fixedDeltaTime,
+            velX * speed,
             rb2D.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {        
+        hasJumped = false;
     }
 }
