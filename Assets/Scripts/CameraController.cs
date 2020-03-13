b145ed813bfcbb989b43d5b4c339fb2b63670681
Mirrorworld <ba405@cam.ac.uk>
2020-03-13 14:58:40 +0000

    :(

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(
+            player.position.x, 
+            player.position.y,
+            transform.position.z);
+    }
+}
